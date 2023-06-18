21d7f466862e93e6fe8926ab77afdd1dfb778df3
Maxym_Tomus <101803422+mrmarkiz@users.noreply.github.com>
2023-06-18 14:21:49 +0300

    added dz1

--- dz1/Form1.Designer.cs
+++ dz1/Form1.Designer.cs
@@ -28,12 +28,120 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            components = new System.ComponentModel.Container();
+            dateTimePicker1 = new DateTimePicker();
+            button1 = new Button();
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            textBoxSeconds = new TextBox();
+            textBoxMinutes = new TextBox();
+            textBoxHours = new TextBox();
+            timer1 = new System.Windows.Forms.Timer(components);
+            SuspendLayout();
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(25, 12);
+            dateTimePicker1.MinDate = new DateTime(2023, 6, 18, 0, 0, 0, 0);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(257, 27);
+            dateTimePicker1.TabIndex = 0;
+            dateTimePicker1.Value = new DateTime(2023, 6, 18, 0, 0, 0, 0);
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.Location = new Point(126, 65);
+            button1.Name = "button1";
+            button1.Size = new Size(156, 180);
+            button1.TabIndex = 13;
+            button1.Text = "Set up timer";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(34, 195);
+            label3.Name = "label3";
+            label3.Size = new Size(64, 20);
+            label3.TabIndex = 12;
+            label3.Text = "Seconds";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(37, 124);
+            label2.Name = "label2";
+            label2.Size = new Size(61, 20);
+            label2.TabIndex = 11;
+            label2.Text = "Minutes";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(39, 58);
+            label1.Name = "label1";
+            label1.Size = new Size(48, 20);
+            label1.TabIndex = 10;
+            label1.Text = "Hours";
+            // 
+            // textBoxSeconds
+            // 
+            textBoxSeconds.Location = new Point(25, 218);
+            textBoxSeconds.Name = "textBoxSeconds";
+            textBoxSeconds.Size = new Size(82, 27);
+            textBoxSeconds.TabIndex = 9;
+            // 
+            // textBoxMinutes
+            // 
+            textBoxMinutes.Location = new Point(25, 147);
+            textBoxMinutes.Name = "textBoxMinutes";
+            textBoxMinutes.Size = new Size(82, 27);
+            textBoxMinutes.TabIndex = 8;
+            // 
+            // textBoxHours
+            // 
+            textBoxHours.Location = new Point(25, 81);
+            textBoxHours.Name = "textBoxHours";
+            textBoxHours.Size = new Size(82, 27);
+            textBoxHours.TabIndex = 7;
+            // 
+            // timer1
+            // 
+            timer1.Interval = 1000;
+            timer1.Tick += timer1_Tick;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(308, 259);
+            Controls.Add(button1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(textBoxSeconds);
+            Controls.Add(textBoxMinutes);
+            Controls.Add(textBoxHours);
+            Controls.Add(dateTimePicker1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private DateTimePicker dateTimePicker1;
+        private Button button1;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private TextBox textBoxSeconds;
+        private TextBox textBoxMinutes;
+        private TextBox textBoxHours;
+        private System.Windows.Forms.Timer timer1;
     }
 }
--- dz1/Form1.cs
+++ dz1/Form1.cs
@@ -2,9 +2,35 @@ namespace dz1
 {
     public partial class Form1 : Form
     {
+        int hours;
+        int minutes;
+        int seconds;
+        DateTime date;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int.TryParse(textBoxHours.Text, out hours);
+            int.TryParse(textBoxMinutes.Text, out minutes);
+            int.TryParse(textBoxSeconds.Text, out seconds);
+            date = dateTimePicker1.Value;
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            DateTime now = DateTime.Now;
+            if (date.ToShortDateString() != now.ToShortDateString())
+                return;
+            if (now.Hour == hours && now.Minute == minutes && now.Second == seconds)
+            {
+                ((System.Windows.Forms.Timer)sender).Stop();
+                MessageBox.Show("Timer !!!", "Time's up!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
         }
     }
 }
