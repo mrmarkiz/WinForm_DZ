429a7acf2926e77dd90052f2c7d46df37b58d633
mrmarkiz <maxtomusiak315@gmail.com>
2023-07-01 12:49:05 +0300

    edited dz1

--- dz1/Form1.cs
+++ dz1/Form1.cs
@@ -2,10 +2,10 @@ namespace dz1
 {
     public partial class Form1 : Form
     {
-        int hours;
-        int minutes;
-        int seconds;
-        DateTime date;
+        private int _hours;
+        private int _minutes;
+        private int _seconds;
+        private DateTime _date;
 
         public Form1()
         {
@@ -14,31 +14,31 @@ namespace dz1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            if (!int.TryParse(textBoxHours.Text, out hours))
+            if (!int.TryParse(textBoxHours.Text, out _hours))
             {
                 MessageBox.Show($"Incorrect or empty text in field: {label1.Text}");
                 return;
             }
-            if (!int.TryParse(textBoxMinutes.Text, out hours))
+            if (!int.TryParse(textBoxMinutes.Text, out _hours))
             {
                 MessageBox.Show($"Incorrect or empty text in field: {label2.Text}");
                 return;
             }
-            if (!int.TryParse(textBoxSeconds.Text, out hours))
+            if (!int.TryParse(textBoxSeconds.Text, out _hours))
             {
                 MessageBox.Show($"Incorrect or empty text in field: {label3.Text}");
                 return;
             }
-            date = dateTimePicker1.Value;
+            _date = dateTimePicker1.Value;
             timer1.Start();
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
             DateTime now = DateTime.Now;
-            if (date.ToShortDateString() != now.ToShortDateString())
+            if (_date.ToShortDateString() != now.ToShortDateString())
                 return;
-            if (now.Hour == hours && now.Minute == minutes && now.Second == seconds)
+            if (now.Hour == _hours && now.Minute == _minutes && now.Second == _seconds)
             {
                 ((System.Windows.Forms.Timer)sender).Stop();
                 MessageBox.Show("Timer !!!", "Time's up!", MessageBoxButtons.OK, MessageBoxIcon.Information);
